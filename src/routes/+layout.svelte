c11e041e08886f05ca6858f2a0ec20617d57b986
kordicjosip <josip.kordic6@gmail.com>
2023-11-14 21:08:46 +0100

    navbar links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
-    import "../app.css";
+	import '../app.css';
+	import { goto } from '$app/navigation';
 </script>
 
-<div class="h-14 bg-orange-300">navbar</div>
+<div class="h-14 bg-orange-300">
+	navbar
+	<button on:click={() => goto('/')}>home</button>
+	<button on:click={() => goto('/webinars')}>webinars</button>
+</div>
 <slot />
