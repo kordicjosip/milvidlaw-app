1f9aabdfb7c194c2efa0f0e2b91e53b0384a1672
kordicjosip <josip.kordic6@gmail.com>
2023-11-18 20:01:40 +0100

    remove nav:(

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,65 +1,36 @@
 <script>
 	import '../app.css';
-	import logo from '$lib/assets/milvidskiy-logo-2x.png';
+	import logo from '$lib/assets/horizontal-white-400x89.png';
 	import Hamburger from '$lib/Hamburger.svelte';
 	import { slide } from 'svelte/transition';
 
 	let mobileNavOpen = false;
 </script>
 
-<div class="flex lg:items-center justify-between lg:justify-around w-full p-6">
+<div
+	class="flex gap-5 lg:gap-0 flex-col lg:flex-row items-center bg-plava lg:justify-around w-full p-6"
+>
 	<a class="" href="https://milvidlaw.com/">
 		<img class="max-h-20 lg:h-20 h-12" src={logo} alt="Milvidskiy Law Group P.C. Logo" title="" />
 	</a>
-	<div class="block lg:hidden"><Hamburger bind:open={mobileNavOpen} /></div>
-	<div class="lg:flex flex-col items-center hidden">
+	<div class="flex flex-col items-center">
 		<a
-			class="flex flex-row items-center text-plava font-bold text-center text-2xl"
+			class="flex flex-row items-center text-white font-bold text-center text-3xl"
 			href="tel:8773103939"
 			><svg height="26" width="26" transform="rotate(90)" viewBox="0 0 32 32"
 				><path
-					fill="#0b3259"
+					fill="#fff"
 					fill-rule="evenodd"
 					d="M31.073 4.433l-2.878-2.87a2.038 2.038 0 00-2.878 0L21 7.303c-.659.971-.794 2.077 0 2.87l1.762 1.757c-1.306 1.75-2.874 3.658-4.639 5.418-1.996 1.99-4.216 3.799-6.212 5.285l-1.703-1.697c-.794-.793-1.903-.659-2.878 0l-5.756 4.305c-.965.665-.795 2.077 0 2.87l2.878 2.869c1.59 1.586 3.657 1.078 5.756 0 0 0 6.353-3.556 11.792-8.98 5.116-5.102 9.073-11.827 9.073-11.827.825-2.263 1.59-4.155 0-5.74"
 				/></svg
 			>(877) 310-3939</a
 		>
-		<div class="mt-2 text-[1rem] uppercase font-[400] text-[#1c1c1c]" style="text-align:center">
-			Elder Law &amp; Estate Planning
+		<div class="mt-2 text-[1rem] uppercase font-[400] text-white" style="text-align:center">
+			ELDER LAW | ESTATE PLANNING | SPECIAL NEEDS
 		</div>
 	</div>
 </div>
-{#if mobileNavOpen}
-	<div class="bg-[#f5f5f5] lg:hidden" transition:slide={{ duration: 300 }}>
-		<nav class="mobilenav">
-			<ul class="p-8">
-				<li><a href="https://milvidlaw.com">Home</a></li>
-				<li><a href="https://milvidlaw.com/our-team">Our Team</a></li>
-				<li><a href="https://milvidlaw.com/services">Services</a></li>
-				<li><a href="/">Webinars</a></li>
-				<li><a href="https://milvidlaw.com/blog">Blog</a></li>
-				<li><a href="https://milvidlaw.com/contact">Contact Us</a></li>
-			</ul>
-		</nav>
-	</div>
-{/if}
 
-<div class="lg:flex hidden w-full h-16 bg-plava text-white items-center justify-around">
-	<nav class="stroke" id="mainNav">
-		<ul class="flex gap-8">
-			<li><a class="stroke" href="https://milvidlaw.com">Home</a></li>
-			<li><a class="" href="https://milvidlaw.com/our-team">Our Team</a></li>
-			<li><a class="" href="https://milvidlaw.com/services">Services</a></li>
-			<li>
-				<a class="text-zuta border border-transparent border-b-zuta border-b-2" href="/">Webinars</a
-				>
-			</li>
-			<li><a class="" href="https://milvidlaw.com/blog">Blog</a></li>
-			<li><a class="" href="https://milvidlaw.com/contact">Contact Us</a></li>
-		</ul>
-	</nav>
-	<div />
-</div>
 <slot />
 
 <style>
