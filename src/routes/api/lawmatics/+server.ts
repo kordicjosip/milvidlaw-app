5658111234e6085046b71343b9a8f228ee26dfd7
Josip <josip.kordic6@gmail.com>
2023-11-21 16:00:41 +0100

    mozda

--- src/routes/api/lawmatics/+server.ts
+++ src/routes/api/lawmatics/+server.ts
@@ -1,15 +1,13 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const req = await request.json();
-
 	const response = await fetch('https://api.lawmatics.com/v1/prospects', {
 		method: 'POST',
 		headers: {
 			Authorization: 'Bearer fltZZ1DoC44K2s2wSXVcU_nOf-XjGadcfv52OgUYgs0',
 			Accepts: 'application/json'
 		},
-		body: JSON.stringify(req),
+		body: await request.text(),
 		redirect: 'follow'
 	});
 
