27ecaf62d8c69ad5836eb7448352214a45531350
kordicjosip <josip.kordic6@gmail.com>
2023-11-27 00:14:27 +0100

    intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,9 +62,19 @@
 </svelte:head>
 <div class="flex flex-col">
 	<div class="flex flex-col gap-6 w-full items-center my-5 lg:my-10">
-		<div class="flex justify-start items-center w-[22rem] lg:w-[43rem] text-3xl font-bold mb-4">
-			<div class="pl-6 border-b-[5px] border-[#dd2444]">UPCOMING EVENTS</div>
+		<div class="flex flex-col justify-start items-center w-[22rem] lg:w-[43rem] mb-4">
+			<div class="pl-6 border-b-[5px] border-[#dd2444] text-3xl font-bold mb-4">
+				UPCOMING EVENTS
+			</div>
+			<div class="">
+				Our attorneys host a series of free educational webinars focusing on such key areas as elder
+				law, estate planning, wills and trusts, asset protection, long-term care and Medicaid
+				planning. These sessions are designed to inform and empower you with essential legal
+				knowledge. Whether you're planning for the future or navigating current legal challenges,
+				our webinars offer guidance and clarity.
+			</div>
 		</div>
+
 		{#each Object.entries(groupedWebinars) as [date, webinarsInDateGroup]}
 			<section class="flex flex-col gap-2">
 				<span class="font-medium text-lg">{date}</span>
